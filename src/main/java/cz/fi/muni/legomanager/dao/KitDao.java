fb55a015856a9100da2a6bcce072355a49e98eb5
Martin Jordán <martin@jordan.cz>
2017-10-28 16:53:57 +0200

    update KitDaoImpl and KitDao

--- src/main/java/cz/fi/muni/legomanager/dao/KitDao.java
+++ src/main/java/cz/fi/muni/legomanager/dao/KitDao.java
@@ -9,7 +9,6 @@ import cz.fi.muni.legomanager.entity.Kit;
 public interface KitDao {
     public void create(Kit kit);
     public Kit findById(Long id);
-    public void update(Long id, String description, Integer price, Integer ageLimit);
+    public void update(Kit kit);
     public void delete(Kit kit);
-
 }
