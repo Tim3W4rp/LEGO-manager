d6618ed472e5a0b2de734c5fd705bcc471a5b8a2
Martin Jordán <martin@jordan.cz>
2017-10-30 13:58:27 +0100

    add base tests

--- src/test/java/cz/fi/muni/legomanager/dao/CategoryDaoTest.java
+++ src/test/java/cz/fi/muni/legomanager/dao/CategoryDaoTest.java
@@ -2,11 +2,8 @@ package cz.fi.muni.legomanager.dao;
 
 import cz.fi.muni.legomanager.PersistenceSampleApplicationContext;
 import cz.fi.muni.legomanager.entity.Category;
-import cz.fi.muni.legomanager.entity.Kit;
-import cz.fi.muni.legomanager.entity.SetOfKits;
 import org.hibernate.Session;
 import org.junit.Assert;
-import org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,10 +14,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
-import org.junit.Assert.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * Test of {@link CategoryDao} methods.
@@ -39,50 +39,57 @@ public class CategoryDaoTest extends AbstractTestNGSpringContextTests {
     @PersistenceContext
     private EntityManager em;
 
-    private SetOfKits buildingsSet;
-    private SetOfKits carsSet;
-
     private Category testCategory;
-    private Category carsCategory;
-    private Category boysCategory;
-
-    private Kit eiffelTowerKit;
-    private Kit statueOfLibertyKit;
-    private Kit towerBridgeKit;
-
-    private Kit porscheKit;
-    private Kit ferrariKit;
-    private Kit footballerKit;
+    private Category starWarsCategory;
+    private List<Category> categoryList;
 
     @Before
     public void setUp() throws Exception {
-        testCategory = new Category("Star Wars","A universe of Jedis and space battles");
+        starWarsCategory = new Category("Star Wars","A universe of Jedis and space battles");
+        testCategory = new Category("Test","Test category");
         categoryDao.create(testCategory);
     }
 
     @Test
     public void create() throws Exception {
+        categoryDao.create(starWarsCategory);
         Session session = (Session) em.getDelegate();
-        Category category = (Category) session.createQuery("FROM Category ").list().get(0);
+        Category category = (Category) session.createQuery("FROM Category ").list().get(1);
         Assert.assertEquals(category.getName(), "Star Wars");
     }
 
     @Test
     public void findById() throws Exception {
-        Category category = categoryDao.findById(this.testCategory.getId());
-        Assert.assertEquals(category.getId().longValue(), 1L);
+        Category category = categoryDao.findById(testCategory.getId());
+        assertEquals(category, testCategory);
     }
 
     @Test
     public void update() throws Exception {
+        Session session = (Session) em.getDelegate();
+        testCategory.setDescription("Changed test category");
+        categoryDao.update(testCategory);
+        Category b_db = (Category) session.createQuery("FROM Category ").list().get(0);
+        Assert.assertEquals(b_db.getDescription(), "Changed test category");
     }
 
     @Test
     public void delete() throws Exception {
+        categoryDao.delete(testCategory);
+        Session session = (Session) em.getDelegate();
+        int tableSize = session.createQuery("FROM Category").list().size();
+        Assert.assertEquals(tableSize, 0);
     }
 
     @Test
     public void findAll() throws Exception {
+        categoryList = categoryDao.findAll();
+        Assert.assertEquals(categoryList.size(), 1);
+    }
+
+    @Test
+    public void createNull() throws Exception {
+        categoryDao.create(null);
     }
 
 }
