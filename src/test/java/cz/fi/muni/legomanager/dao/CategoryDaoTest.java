281d10e4f4123e5ca257ea3f03eb6e057ca158ca
Martin Jordán <martin@jordan.cz>
2017-10-30 11:57:46 +0100

    tests for create() and findById()

--- src/test/java/cz/fi/muni/legomanager/dao/CategoryDaoTest.java
+++ src/test/java/cz/fi/muni/legomanager/dao/CategoryDaoTest.java
@@ -2,6 +2,12 @@ package cz.fi.muni.legomanager.dao;
 
 import cz.fi.muni.legomanager.PersistenceSampleApplicationContext;
 import cz.fi.muni.legomanager.entity.Category;
+import cz.fi.muni.legomanager.entity.Kit;
+import cz.fi.muni.legomanager.entity.SetOfKits;
+import org.hibernate.Session;
+import org.junit.Assert;
+import org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +17,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
+import org.junit.Assert.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 /**
- * @author Stepan Granat
+ * Test of {@link CategoryDao} methods.
+ *
+ * @author Martin Jordán
  */
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=PersistenceSampleApplicationContext.class)
 @TestExecutionListeners(TransactionalTestExecutionListener.class)
@@ -31,10 +39,50 @@ public class CategoryDaoTest extends AbstractTestNGSpringContextTests {
     @PersistenceContext
     private EntityManager em;
 
-    @Test
-    public void testCreate() {
-        Category c = new Category("TestName", "Test description");
+    private SetOfKits buildingsSet;
+    private SetOfKits carsSet;
 
-        categoryDao.create(c);
+    private Category testCategory;
+    private Category carsCategory;
+    private Category boysCategory;
+
+    private Kit eiffelTowerKit;
+    private Kit statueOfLibertyKit;
+    private Kit towerBridgeKit;
+
+    private Kit porscheKit;
+    private Kit ferrariKit;
+    private Kit footballerKit;
+
+    @Before
+    public void setUp() throws Exception {
+        testCategory = new Category("Star Wars","A universe of Jedis and space battles");
+        categoryDao.create(testCategory);
     }
+
+    @Test
+    public void create() throws Exception {
+        Session session = (Session) em.getDelegate();
+        Category category = (Category) session.createQuery("FROM Category ").list().get(0);
+        Assert.assertEquals(category.getName(), "Star Wars");
+    }
+
+    @Test
+    public void findById() throws Exception {
+        Category category = categoryDao.findById(this.testCategory.getId());
+        Assert.assertEquals(category.getId().longValue(), 1L);
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void findAll() throws Exception {
+    }
+
 }
