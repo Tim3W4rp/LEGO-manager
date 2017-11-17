ece2735c9390929d5b4af6ff43e0e52b7a2709a3
Martin Jordán <martin@jordan.cz>
2017-11-17 20:13:55 +0100

    change color to RGB, add KitBrick

---
+++ lm-facade/src/main/java/cz/fi/muni/legomanager/dtos/UpdateKitDTO.java
@@ -0,0 +1,11 @@
+package dtos;
+
+/**
+ * @author Martin Jordán
+ */
+public class UpdateKitDTO {
+    public Long id;
+    public String description;
+    public Integer price;
+    public Integer ageLimit;
+}
