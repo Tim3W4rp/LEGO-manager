9f64586b70cf10aa751826a7d7b32736f36a6242
Michal Peška <peskmi@seznam.cz>
2017-11-18 23:14:51 +0100

    SetOfKits service added

--- lm-service/src/main/java/cz/fi/muni/legomanager/services/SetOfKitsService.java
+++ lm-service/src/main/java/cz/fi/muni/legomanager/services/SetOfKitsService.java
@@ -1,4 +1,18 @@
 package cz.fi.muni.legomanager.services;
 
-public class SetOfKitsService {
+import cz.fi.muni.legomanager.entity.SetOfKits;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+/**
+ * @author Michal Peška
+ */
+@Service
+public interface SetOfKitsService {
+    SetOfKits getSet(long id);
+    List<SetOfKits> getAllSets();
+    void create(SetOfKits set);
+    void delete(long id);
+    void update(SetOfKits set);
+
 }
