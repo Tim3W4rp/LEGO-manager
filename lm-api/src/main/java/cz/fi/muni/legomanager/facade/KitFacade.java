7b580323742afb028ecf6ab4639c52f747eb35a7
Martin Jordán <martin@jordan.cz>
2017-11-21 15:00:49 +0100

    correct mapped by

--- lm-api/src/main/java/cz/fi/muni/legomanager/facade/KitFacade.java
+++ lm-api/src/main/java/cz/fi/muni/legomanager/facade/KitFacade.java
@@ -1,8 +1,21 @@
 package cz.fi.muni.legomanager.facade;
 
+import cz.fi.muni.legomanager.dto.KitCreateDTO;
+import cz.fi.muni.legomanager.dto.KitDTO;
+
+import java.util.List;
+
 /**
  * @author Martin Jordán
  */
 public interface KitFacade {
+    public Long createKit(KitCreateDTO kitCreateDTO);
+    public void addCategory(Long kitId, Long categoryId);
+    public void removeCategory(Long kitId, Long categoryId);
+    public void changePrice(Integer newPrice);
+    public void deleteKit();
+    public List<KitDTO> getAllKits();
+    public List<KitDTO> getKitsByCategory();
+    public KitDTO getKitById();
 
 }
