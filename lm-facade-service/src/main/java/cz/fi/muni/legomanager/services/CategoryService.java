ee934ccb96dbd55b690a819aa2d422ba3b1062b9
stepan <granat.stepan@gmail.com>
2017-11-21 12:17:27 +0100

    CategoryService: implement fully

--- lm-facade-service/src/main/java/cz/fi/muni/legomanager/services/CategoryService.java
+++ lm-facade-service/src/main/java/cz/fi/muni/legomanager/services/CategoryService.java
@@ -1,18 +1,31 @@
 package cz.fi.muni.legomanager.services;
 
 import cz.fi.muni.legomanager.entity.Category;
+import cz.fi.muni.legomanager.entity.Kit;
+import cz.fi.muni.legomanager.entity.SetOfKits;
 
 import java.util.List;
 
+/**
+ * @author Stepan Granat
+ */
 public interface CategoryService {
 
     Category getCategory(long id);
 
     List<Category> getAllCategories();
 
-    void create(Category set);
+    void create(Category category);
 
     void delete(long id);
 
     void update(Category category);
+
+    void addKit(Category category, Kit kit);
+
+    void removeKit(Category category, Kit kit);
+
+    void addSetOfKits(Category category, SetOfKits setOfKits);
+
+    void removeSetOfKits(Category category, SetOfKits setOfKits);
 }
--- lm-facade-service/src/main/java/cz/fi/muni/legomanager/services/CategoryServiceImpl.java
+++ lm-facade-service/src/main/java/cz/fi/muni/legomanager/services/CategoryServiceImpl.java
@@ -2,11 +2,15 @@ package cz.fi.muni.legomanager.services;
 
 import cz.fi.muni.legomanager.dao.CategoryDao;
 import cz.fi.muni.legomanager.entity.Category;
+import cz.fi.muni.legomanager.entity.Kit;
+import cz.fi.muni.legomanager.entity.SetOfKits;
 
 import javax.inject.Inject;
 import java.util.List;
 
-
+/**
+ * @author Stepan Granat
+ */
 public class CategoryServiceImpl implements CategoryService {
     private CategoryDao categoryDao;
 
@@ -26,8 +30,8 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public void create(Category set) {
-        categoryDao.create(set);
+    public void create(Category category) {
+        categoryDao.create(category);
     }
 
     @Override
@@ -43,4 +47,32 @@ public class CategoryServiceImpl implements CategoryService {
     public void update(Category category) {
         categoryDao.update(category);
     }
+
+    @Override
+    public void addKit(Category category, Kit kit) {
+        if (category.getKits().contains(kit)) {
+            throw new RuntimeException("Kit is already in category");
+        }
+        category.addKit(kit);
+    }
+
+    @Override
+    public void removeKit(Category category, Kit kit) {
+        category.removeKit(kit);
+    }
+
+    @Override
+    public void addSetOfKits(Category category, SetOfKits setOfKits) {
+        if (category.getSetsOfKits().contains(setOfKits)) {
+            throw new RuntimeException("Set is already in category");
+        }
+        category.addSetOfKits(setOfKits);
+    }
+
+    @Override
+    public void removeSetOfKits(Category category, SetOfKits setOfKits) {
+        category.removeSetOfKits(setOfKits);
+    }
+
+
 }
