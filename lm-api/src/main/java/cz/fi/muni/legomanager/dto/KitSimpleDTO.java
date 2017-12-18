72a06683baaea58e3c2cf7e4262ffb7be27c2065
stepan <granat.stepan@gmail.com>
2017-12-18 21:31:18 +0100

    Category: show related kits

---
+++ lm-api/src/main/java/cz/fi/muni/legomanager/dto/KitSimpleDTO.java
@@ -0,0 +1,66 @@
+package cz.fi.muni.legomanager.dto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Martin Jordán
+ */
+public class KitSimpleDTO {
+    private Long id;
+    private String description;
+    private Integer price;
+    private Integer ageLimit;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(Integer ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        KitSimpleDTO that = (KitSimpleDTO) o;
+
+        if (description != null ? !description.equals(that.description) : that.description != null) return false;
+        if (price != null ? !price.equals(that.price) : that.price != null) return false;
+        return ageLimit != null ? ageLimit.equals(that.ageLimit) : that.ageLimit == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = description != null ? description.hashCode() : 0;
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (ageLimit != null ? ageLimit.hashCode() : 0);
+        return result;
+    }
+}
